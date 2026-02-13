520155d240036ec5933222c4a3462c6bf52449bf
Artyichoku <artyichoku@gmail.com>
2026-02-13 13:14:50 -0300

    feat: setup inicial do projeto

---
+++ api/src/main/java/com/chicohub/api/ChicoHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.chicohub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicoHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicoHubApiApplication.class, args);
+	}
+
+}
